6d2aa986d0ca7028a174ff90f846648b80e3a8d8
Daniel Kholodenko <75667640+dkholode@users.noreply.github.com>
2022-02-28 01:15:33 -0500

    Improved formatting

--- GradeCalcSpreadsheet.py
+++ GradeCalcSpreadsheet.py
@@ -17,17 +17,20 @@ def section(CatCount):
     Points = []
     for i in range(CatCount):
         print('\n')
-        Names.append(input("What is the naming scheme for the assignments? (Ex: HW #) "))
+        Names.append(input("What is the naming scheme for these assignments? (Ex: HW #, Exam #, etc) "))
         Amt.append(input("How many of this assignment type are there? "))
         Points.append(input("How many percent is each assignment worth? "))
     return Amt, Names, Points
 
 def assemble(type, CatCount, Amt, Names, Points):
+    worksheet.set_column(0, 0, 17.75)
     worksheet.write('A1', 'Current Percentage')
     worksheet.write('A4', 'Assignment Name')
     worksheet.write('B4', 'Weight')
     worksheet.write('C4', 'Score')
+
     format = workbook.add_format({'num_format': '0.00%'})
+
     row = 4
     col = 0
     for i in range(CatCount):
@@ -35,18 +38,15 @@ def assemble(type, CatCount, Amt, Names, Points):
         for x in range(int(Amt[i])):
             worksheet.write(row, col, Names[i] + " " + str(AssignmentNum[x]+1))
             row+=1
+
     row = 4
     col = 1
     for i in range(CatCount):
-        AssignmentNum = list(range(int(Amt[i])))
         for x in range(int(Amt[i])):
             worksheet.write(row, col, float(Points[i])/100,format)
             row+=1
-    
     worksheet.write_formula('A2', '{=SUM(B5:B500*C5:C500)/100}', format)
     
-
-
 
 CatCount = terms()
 Amt, Names, Points = section(CatCount)
