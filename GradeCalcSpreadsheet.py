f43320776ab86be47e12f553519f9d83808c5c19
dkholode <dkholode@purdue.edu>
2023-04-04 19:40:38 -0400

    updated functionality and refactored code

--- GradeCalcSpreadsheet.py
+++ GradeCalcSpreadsheet.py
@@ -1,8 +1,5 @@
 import xlsxwriter as x
 
-workbook = x.Workbook('GradeCalc.xlsx')
-worksheet = workbook.add_worksheet()
-
 def getInt(prompt):
     while True:
         try:
@@ -19,15 +16,15 @@ def section(CatCount):
         print('\n')
         Names.append(input("What is the naming scheme for these assignments? (Ex: HW #, Exam #, etc) "))
         Amt.append(getInt("How many of this assignment type are there? "))
-        Points.append(input("How many percent is each assignment worth? "))
+        Points.append(input("How much does each assignment worth? (As a total of %) "))
     return Amt, Names, Points
 
 def assemble(CatCount, Amt, Names, Points):
     worksheet.set_column(0, 0, 17.75)
     worksheet.write('A1', 'Current Percentage')
     worksheet.write('A4', 'Assignment Name')
-    worksheet.write('B4', 'Weight')
-    worksheet.write('C4', 'Score')
+    worksheet.write('B4', 'Score')
+    worksheet.write('C4', 'Weight')
     format = workbook.add_format({'num_format': '0.00%'})
     worksheet.write_formula('A2', '{=SUM(B5:B500*C5:C500)/100}', format)
 
@@ -38,17 +35,18 @@ def assemble(CatCount, Amt, Names, Points):
         for x in range(int(Amt[i])):
             worksheet.write(row, col, Names[i] + " " + str(AssignmentNum[x]+1))
             row+=1
-
     row = 4
-    col = 1
+    col = 2
     for i in range(CatCount):
         for x in range(int(Amt[i])):
             worksheet.write(row, col, float(Points[i])/100,format)
             row+=1
-    
 
-CatCount = getInt("How many categories does your class have? (HW, Exams, Quizzes, etc)? ")
-Amt, Names, Points = section(CatCount)
-assemble(CatCount, Amt, Names, Points)
-
-workbook.close()
+if __name__ == "__main__":
+    Course = input("What course is this for?")
+    workbook = x.Workbook(Course+'.xlsx')
+    worksheet = workbook.add_worksheet()
+    CatCount = getInt("How many categories does your class have? (HW, Exams, Quizzes, etc)? ")
+    Amt, Names, Points = section(CatCount)
+    assemble(CatCount, Amt, Names, Points)
+    workbook.close()
